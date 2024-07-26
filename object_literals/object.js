1a008d60c47f5c1cae0c2937fe6f38a981937176
Anandrajoriya <anandrajoriya03@gmail.com>
2024-07-26 20:31:17 +0530

    object letrals

---
+++ object_literals/object.js
@@ -0,0 +1,33 @@
+//! object are just key value pair and we access data form costom key
+
+const data={
+    name:'aditya',
+    class:'12',
+    age:'19',
+    stream:'cs'
+}
+
+// console.log(`${data.name},${data.class}`);
+
+//! add data
+data.name='anand'
+console.log(data);
+
+// ! object + array
+
+const  shoppingCart=[
+    {
+        product:'mobile',
+        print:98000,
+        quantity:1
+    },
+    {
+        product:'TV',
+        print:59000,
+        quantity:2
+    },{
+        product:'washing machine',
+        print:30000,
+        quantity:1
+    }
+]
