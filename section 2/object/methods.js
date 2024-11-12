9260528dde7db2e6cfa8aca927df4e367a37d199
Anandrajoriya <anandrajoriya03@gmail.com>
2024-11-12 15:13:40 +0530

    methods

--- section 2/object/methods.js
+++ section 2/object/methods.js
@@ -23,6 +23,40 @@ const detail={
         return `aditya is a ${this.age} old ${this.job}`
     }
 }
-console.log(detail.calcAge(2003))
+// console.log(detail.calcAge(2003))
 // console.log(detail['calcAge'](2002));
-console.log(detail.summary());
+// console.log(detail.summary());
+
+
+// question
+
+const mark={
+    fullName:'Mark Miller',
+    mass:78,
+    height:1.69,
+    calcAge:function(){
+        this.bmi=this.mass/(this.height*this.height)
+        return this.bmi
+    }
+}
+const john={
+    fullName:'John Smith',
+    mass:92,
+    height:1.95,
+    calcAge:function(){
+        this.bmi=this.mass/(this.height*this.height)
+        return this.bmi
+    }
+}
+
+mark.calcAge()
+john.calcAge();
+
+if(mark.bmi>john.bmi){
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher then ${john.fullName}'s BMI (${john.bmi})`);
+}else if(mark.bmi<john.bmi){
+    console.log(`${john.bmi}'s BMI (${john.bmi}) is higher then ${mark.fullName}'s BMI (${mark.bmi})`);
+}else{
+    console.log("both bmi is same");
+    
+}
