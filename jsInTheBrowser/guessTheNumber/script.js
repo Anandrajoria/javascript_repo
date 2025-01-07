b714d59375c81edf206d2b1652f86654e179ba81
Anandrajoriya <anandrajoriya03@gmail.com>
2025-01-07 22:13:40 +0530

    updating in porject

--- jsInTheBrowser/guessTheNumber/script.js
+++ jsInTheBrowser/guessTheNumber/script.js
@@ -1,25 +1,45 @@
-'use strict'
+"use strict";
 
-/* //!basic understanding of dom
-console.log(document.querySelector('.message').textContent)
-document.querySelector('.message').textContent='correct number'
+const secretNumber = Math.ceil(Math.random() * 20 + 1);
 
-document.querySelector('.number').textContent=13
-document.querySelector('.score').textContent=23
+let score = 20;
 
-console.log(document.querySelector('.guess').value=23);
-*/
+document.querySelector(".number").textContent = secretNumber;
 
-const number=Math.ceil(Math.random()*20+1)
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess);
 
+  if (!guess) {
+    //when their is no input
+    document.querySelector(".message").textContent = "🚫no number";
 
-document.querySelector('.number').textContent=number
+  } else if (guess === secretNumber) {
+    //when correcting the right guess
+    document.querySelector(".message").textContent = "🎉correct number";
+    document.querySelector('body').style.backgroundColor='#60b347'
+  } else if (guess > secretNumber) {
+    //when guess it lower then actual value
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }else{
+        document.querySelector('.message').textContent='you lost the game'
+      document.querySelector(".score").textContent = 0;
 
-document.querySelector('.check').addEventListener('click',function(){
-    const guess=Number(document.querySelector('.guess').value);
-        console.log( typeof guess);
-        
-    if(!guess){
-        document.querySelector('.message').textContent='🚫no number'
     }
-})
+  } else if (guess < secretNumber) {
+    //when guess is higher then actual value
+    if (score > 1) {
+        document.querySelector(".message").textContent = "📈too low";
+        score--;
+        document.querySelector(".score").textContent = score;
+      }else{
+          document.querySelector('.message').textContent='you lost the game'
+        document.querySelector(".score").textContent = 0;
+  
+      }
+      
+  }
+});
