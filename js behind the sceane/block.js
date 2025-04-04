41d278912274b6419e7e8d05a5cf23168cfd9c5e
Anandrajoriya <anandrajoriya03@gmail.com>
2025-04-04 23:58:36 +0530

    block

--- js behind the sceane/block.js
+++ js behind the sceane/block.js
@@ -1,16 +1,33 @@
-var a=100
 
-{
-  let a = 10;
-  var b = 20;
-  const c = 30;
+/*
+if (true) {
+  // compound statement
+  var a=10
   console.log(a);
-  console.log(b);
-  console.log(c);
+  
 }
+*/
 
+
+
+/*const c=100
+
+function a(){
+  var a=10
+  let b=20
+  const c=30
+  console.log(c);
+  
+}
+a()
+console.log(c);
+*/
+
+
+let a=20
+function x(){
+  var a=10;
+  console.log(a);
+}
+x() 
 console.log(a);
-
-// console.log(b);
-// console.log(a);
-// console.log(c);
