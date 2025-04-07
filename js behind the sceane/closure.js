d7ff45504b14f939a4dccc65986bdd12afe954ea
Anandrajoriya <anandrajoriya03@gmail.com>
2025-04-07 22:28:29 +0530

    closures

--- js behind the sceane/closure.js
+++ js behind the sceane/closure.js
@@ -1,13 +1,14 @@
-function x(){
-  var a=7
+function z() {
+  var b = 900;
+  function x() {
+    var a = 7;
 
-  function y(){
-    console.log(a);
+    function y() {
+      console.log(a, b);
+    }
+    y()
   }
-  a=100;
-  return y
+  x()
 }
 
-let z=x()
-console.log(z);
 z()
