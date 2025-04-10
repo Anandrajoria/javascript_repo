4337f7d875c4fddb1c47fcb067d5bbf68cdf4c57
Anandrajoriya <anandrajoriya03@gmail.com>
2025-04-10 19:01:14 +0530

    higer order function

--- js behind the sceane/higherOrderFunction.js
+++ js behind the sceane/higherOrderFunction.js
@@ -7,3 +7,67 @@ function y(x) {
   x();
 }
 
+// map , filter and reduce
+const arr = [5, 1, 3, 2, 6];
+/*
+function double(x) {
+  return x * 2;
+}
+
+function binary(x){
+  return x.toString(2)
+}
+
+const output=arr.map(binary)
+console.log(output);
+*/
+// console.log(arr.map((e)=>e*2));
+
+//! filter
+
+// console.log(arr.filter((e)=>e%2!=0));
+
+//! reduce
+// console.log(arr.reduce((acc,cur)=>acc+cur));
+
+//! find the max element
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+// console.log(findMax(arr));
+
+const output = arr.reduce((max, cur) => {
+  if (cur > max) {
+    max = cur;
+  }
+  return max;
+}, 0);
+
+// console.log(output);
+
+
+const users=[
+  {firstName:'adi',lastName:'pr',age:12},
+  {firstName:'rajjo',lastName:'ksris',age:62},
+  {firstName:'sud',lastName:'mon',age:52},
+  {firstName:'siy',lastName:'jyo',age:42},
+]
+
+const output2=users.map(x=>x.firstName +" "+ x.lastName)
+// console.log(output2);
+
+const output3=users.reduce((acc,cur)=>{
+  if(acc[cur.age]){
+    acc[cur.age]=++acc[cur+age]
+  }else{
+    acc[cur.age]=1
+  }
+  return acc
+},{})
+console.log(output3);
