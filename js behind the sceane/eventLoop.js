03f46a62428cf6246aea33263ee052ad305a141d
Anandrajoriya <anandrajoriya03@gmail.com>
2025-04-29 22:01:27 +0530

    event loop

--- js behind the sceane/eventLoop.js
+++ js behind the sceane/eventLoop.js
@@ -1,4 +1,4 @@
-console.log("start");
+/*console.log("start");
 
 setTimeout(function cbT() {
   console.log("cb set time out");
@@ -8,3 +8,15 @@ fetch("https://api.netflix.com").then(function cbF() {
   console.log("CB netflix");
 });
 console.log("end");
+*/
+
+function main() {
+  console.log("a");
+  setTimeout(function display() {
+    console.log("b");
+  }, 0);
+  console.log('c');
+  
+}
+
+main();
