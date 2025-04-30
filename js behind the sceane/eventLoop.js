344296fdc5f2b9a59d5a1ddc6ccaadc531fe3446
Anandrajoriya <anandrajoriya03@gmail.com>
2025-04-30 23:06:50 +0530

    event loop

--- js behind the sceane/eventLoop.js
+++ js behind the sceane/eventLoop.js
@@ -10,6 +10,7 @@ fetch("https://api.netflix.com").then(function cbF() {
 console.log("end");
 */
 
+/*
 function main() {
   console.log("a");
   setTimeout(function display() {
@@ -18,5 +19,28 @@ function main() {
   console.log('c');
   
 }
-
 main();
+*/
+
+function main() {
+  console.log("a");
+
+  setTimeout(function exec() {
+    console.log("b");
+  }, 0);
+
+  runWhileLoopForNSecond(3);
+  console.log("c");
+}
+
+main()
+
+function runWhileLoopForNSecond(sec){
+  let start=Date.now() ,now=start
+
+  while(now-start < (sec*1000)){
+    now=Date.now()
+  }
+}
+
+//So the delay argument in setTimeout() does not guarantee the start of execution after timer finishes the delay. It serves as a minimum time for the delay part.
