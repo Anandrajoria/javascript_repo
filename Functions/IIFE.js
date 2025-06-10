ebf92a1d8e99b05d0b837afb0bfb9a2cadb2ca7b
Anandrajoriya <anandrajoriya03@gmail.com>
2025-06-10 16:16:05 +0530

    iife

--- Functions/IIFE.js
+++ Functions/IIFE.js
@@ -1,10 +1,21 @@
 const runOnce = function () {
-  console.log("this will never run again");
+  console.log("this can run again");
 };
 
 runOnce();
 
+//IIFE
 (function () {
   console.log("this will never run again");
 })();
 (() => console.log("this also"))();
+
+
+var somethingElse=(function(nm){
+  return {
+    'name':nm,
+    'id':123
+  }
+})('bob')
+
+console.log(somethingElse);
